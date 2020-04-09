77a99c2b45245ad71b8d8898edcdcf5a4450421f
btylk <55312210+btylk@users.noreply.github.com>
2020-04-09 23:35:34 +0700

    add main icon

---
+++ PlayerGui.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class PlayerGui extends JFrame{
+
+    public PlayerGui(){
+        super("Main Menu");
+        Container container = getContentPane();
+        container.setLayout(new GridLayout(3,1));
+
+        JPanel p1 = new JPanel();
+        ImageIcon iconlogo = new ImageIcon("Logo3.png");
+        JLabel logo = new JLabel(iconlogo);
+        p1.add(logo);
+        //p1.setLayout(new FlowLayout());
+        container.add(p1);
+
+        JPanel p2 = new JPanel();
+        ImageIcon startlogo = new ImageIcon("startLogo2.png");
+        Image startImage = startlogo.getImage();
+        Image modifiedstartImage = startImage.getScaledInstance(125, 67, java.awt.Image.SCALE_SMOOTH);
+        startlogo = new ImageIcon(modifiedstartImage);
+        JLabel slogo = new JLabel(startlogo);
+        p2.add(slogo);
+        //p2.setLayout(new FlowLayout());
+        container.add(p2);
+
+        JPanel p3 = new JPanel();
+        ImageIcon playerIcon = new ImageIcon("pLogo.png");
+        /*Image playerImage = playerIcon.getImage();
+        Image modifiedplayerImage = playerImage.getScaledInstance(125, 175, java.awt.Image.SCALE_SMOOTH);
+        playerIcon = new ImageIcon(modifiedplayerImage);*/
+        JLabel pLogo = new JLabel(playerIcon);
+
+        p3.add(pLogo);
+        //p3.setLayout(new FlowLayout());
+        container.add(p3);
+
+    
+    
+
+
+
+
+
+        setSize(600,600);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args){
+        new PlayerGui();
+    }
+}
